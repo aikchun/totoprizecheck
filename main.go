fad5442f17c861da17e404c5b560a8664584bd30
aikchun <iam@simaikchun.com>
2023-05-20 17:45:56 +0800

    Added NewTotoDraw and added validations with tests.

--- main.go
+++ main.go
@@ -1,31 +1,89 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"sort"
 	"strconv"
 	"strings"
 )
 
+type TotoDraw struct {
+	WinningNumbers   []int
+	AdditionalNumber int
+}
+
+func NewTotoDraw(numbers string, a string) (TotoDraw, error) {
+	sortedNumbers, err := convertStringToUniqueSortedNumbers(numbers)
+
+	errorPreText := "NewTotoDraw error:"
+
+	if err != nil {
+		return TotoDraw{}, fmt.Errorf("%s %s", errorPreText, err)
+	}
+
+	if len(sortedNumbers) != 6 {
+		return TotoDraw{}, fmt.Errorf("%s winning numbers should only have a length of 6", errorPreText)
+	}
+
+	addNum, err := convertStringToNumber(a)
+
+	if err != nil {
+		return TotoDraw{}, fmt.Errorf("unable to convert additional number: %s", a)
+	}
+
+	for _, n := range sortedNumbers {
+		if n == addNum {
+			return TotoDraw{}, fmt.Errorf("duplicate number found in additional number")
+		}
+	}
+
+	n := TotoDraw{
+		WinningNumbers:   sortedNumbers,
+		AdditionalNumber: addNum,
+	}
+
+	return n, nil
+}
+
 func main() {
 	fmt.Println("Hello, world.")
-	result, _ := ConvertStringToSortedNumbers("01 2 3 ")
+	result, _ := convertStringToUniqueSortedNumbers("01 2 3 ")
 	fmt.Printf("result %v\n", result)
 }
 
-func ConvertStringToSortedNumbers(str string) ([]int, error) {
+func convertStringToUniqueSortedNumbers(str string) ([]int, error) {
+	errorPreText := "convertStringToUniqueSortedNumbers error:"
 	trimmed := strings.Trim(str, " ")
 	split := strings.Split(trimmed, " ")
+	numberMap := make(map[int]int, len(split))
+
 	var numbers []int
 
 	for _, s := range split {
-		num, err := strconv.Atoi(s)
+		num, err := convertStringToNumber(s)
 		if err != nil {
-			return []int{}, errors.New(fmt.Sprintf("error converting %s, in string: %s", s, split))
+			return []int{}, fmt.Errorf("%s fail to convert %s, in string: %s", errorPreText, s, split)
 		}
+
+		_, ok := numberMap[num]
+		if ok {
+			return []int{}, fmt.Errorf("%s should not have duplicate numbers", errorPreText)
+		}
+
+		numberMap[num] = 1
+
 		numbers = append(numbers, num)
-
 	}
-	return numbers, nil
 
+	sort.Ints(numbers)
+
+	return numbers, nil
+}
+
+func convertStringToNumber(str string) (int, error) {
+	num, err := strconv.Atoi(str)
+	if err != nil {
+		return 0, err
+	}
+	return num, err
 }
--- main_test.go
+++ main_test.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
-func TestConvertStringToSortedNumbers(t *testing.T) {
-	input := "01 2 3 "
-	numbers, err := ConvertStringToSortedNumbers(input)
+func TestConvertStringToSortedNumbersSpaceTrim(t *testing.T) {
+	input := " 1 2 3 "
+	numbers, err := convertStringToUniqueSortedNumbers(input)
 
 	if err != nil {
 		t.Errorf("unexpected error converting %s", input)
@@ -18,6 +19,107 @@ func TestConvertStringToSortedNumbers(t *testing.T) {
 		if numbers[i] != num {
 			t.Errorf("was expecting %v but got %v instead", expected, numbers)
 		}
-
 	}
+}
+
+func TestConvertStringToSortedNumbersLeading0(t *testing.T) {
+	input := "01 02 03"
+	numbers, err := convertStringToUniqueSortedNumbers(input)
+
+	if err != nil {
+		t.Errorf("unexpected error converting %s", input)
+	}
+
+	expected := []int{1, 2, 3}
+
+	for i, num := range expected {
+		if numbers[i] != num {
+			t.Errorf("was expecting %v but got %v instead", expected, numbers)
+		}
+	}
+}
+
+func TestConvertStringToSortedNumbersSorted(t *testing.T) {
+	input := "3 2 1"
+	numbers, err := convertStringToUniqueSortedNumbers(input)
+
+	if err != nil {
+		t.Errorf("unexpected error converting %s", input)
+	}
+
+	expected := []int{1, 2, 3}
+
+	for i, num := range expected {
+		if numbers[i] != num {
+			t.Errorf("was expecting %v but got %v instead", expected, numbers)
+		}
+	}
+}
+
+func TestConvertStringToNumber(t *testing.T) {
+	input := "01"
+	number, err := convertStringToNumber(input)
+
+	if err != nil {
+		t.Errorf("unexpected error converting %s", input)
+	}
+
+	expected := 1
+
+	if number != expected {
+		t.Errorf("expecting %d got %d instead", expected, number)
+	}
+}
+
+func TestNewTotoDraw(t *testing.T) {
+	inputWinningNumbers := "7 13 18 19 25 29"
+	inputAdditionalNumber := "36"
+
+	totoDraw, err := NewTotoDraw(inputWinningNumbers, inputAdditionalNumber)
+
+	if err != nil {
+		t.Errorf("error in NewTotoDraw %v", err)
+	}
+
+	expectedWinningNumbers := []int{7, 13, 18, 19, 25, 29}
+
+	for i, n := range expectedWinningNumbers {
+		if n != totoDraw.WinningNumbers[i] {
+			t.Errorf("was expecting %d but got %d instead", n, totoDraw.WinningNumbers[i])
+		}
+	}
+
+	expectedAdditionalNumber := 36
+	if totoDraw.AdditionalNumber != expectedAdditionalNumber {
+		t.Errorf("was expecting %d but got %d instead", expectedAdditionalNumber, totoDraw.AdditionalNumber)
+	}
+
+}
+
+func TestNewTotoDrawWrongWinningNumberLength(t *testing.T) {
+	inputWinningNumbers := "7 13 18 19 25 29 30"
+	inputAdditionalNumber := "36"
+
+	_, err := NewTotoDraw(inputWinningNumbers, inputAdditionalNumber)
+
+	expectedErrorString := "NewTotoDraw error: winning numbers should only have a length of 6"
+	actualErrorString := fmt.Sprint(err)
+	if actualErrorString != expectedErrorString {
+		t.Errorf("expected '%s' but got: '%s' instead", expectedErrorString, actualErrorString)
+	}
+
+}
+
+func TestNewTotoDrawDuplicateWinningNumber(t *testing.T) {
+	inputWinningNumbers := "7 13 18 19 29 29"
+	inputAdditionalNumber := "36"
+
+	_, err := NewTotoDraw(inputWinningNumbers, inputAdditionalNumber)
+
+	expectedErrorString := "NewTotoDraw error: convertStringToUniqueSortedNumbers error: should not have duplicate numbers"
+	actualErrorString := fmt.Sprint(err)
+	if actualErrorString != expectedErrorString {
+		t.Errorf("expected '%s' but got: '%s' instead", expectedErrorString, actualErrorString)
+	}
+
 }
